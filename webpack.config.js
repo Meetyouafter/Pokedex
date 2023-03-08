e3b7f3baac8d4d33a5ec98a8009d75cede07be36
Levis <anton_levus@mail.ru>
2023-03-08 11:55:49 +0530

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,8 @@ module.exports = {
   },
   optimization: optimization(),
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
     hot: isDev,
     host: '0.0.0.0',
     port: 8080,
